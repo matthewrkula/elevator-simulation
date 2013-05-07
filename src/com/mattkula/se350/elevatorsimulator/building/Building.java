2aeb3e9ce11a936670d2ce37c9789bf6fe206a61
Matt Kula <matthewrkula@gmail.com>
2013-05-07 17:55:05 -0500

    Removed extra files

--- src/com/mattkula/se350/elevatorsimulator/building/Building.java
+++ src/com/mattkula/se350/elevatorsimulator/building/Building.java
@@ -2,19 +2,14 @@ package com.mattkula.se350.elevatorsimulator.building;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Random;
 
 import com.mattkula.se350.elevatorsimulator.elevator.Elevator;
 import com.mattkula.se350.elevatorsimulator.elevator.ElevatorFactory;
 import com.mattkula.se350.elevatorsimulator.exceptions.InvalidArgumentException;
 
-
-
 public class Building {
 	
 	ArrayList<Elevator> elevators;
-	
-	Random r; // TODO remove
 	
 	public Building(int numOfFloors, int numOfElevators) throws InvalidArgumentException{
 		FloorManager.initialize(numOfFloors);
@@ -43,6 +38,10 @@ public class Building {
 		}
 	}
 	
+	/**
+	 * Keeps track of the Global time, useful for logging.
+	 * @return A String formatting of the current hour, minute, second in hh:mm:ss format
+	 */
 	public static String getTimeString(){
 		Calendar c = Calendar.getInstance();
 		return String.format("%02d:%02d:%02d ", c.get(Calendar.HOUR), c.get(Calendar.MINUTE), c.get(Calendar.SECOND));
